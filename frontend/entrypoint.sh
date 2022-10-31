7b3df6a8c48e4eaf0ebdf97b74a4f30684a2df38
Arthur Gregório <arthurshakal@gmail.com>
2022-10-30 21:38:15 -0300

    fix #3

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
+NGINX_CONF_FILE="/etc/nginx/nginx.conf"
 JS_FOLDER="/app/js/*.js"
 
-EXISTING_VARS=$(printenv | awk -F= '{print $1}' | sed 's/^/\$/g' | paste -sd,)
-export EXISTING_VARS;
-
+# replace placeholders on vue js files
 for file in $JS_FOLDER;
 do
-  sed -i 's/API_PROTOCOL/'${API_PROTOCOL}'/g' $file
-  sed -i 's/API_HOST/'${API_HOST}'/g' $file
+  sed -i 's/WEB_PORT/'${WEB_PORT}'/g' $file
   sed -i 's/"LOG_REQUESTS"/'${LOG_REQUESTS}'/g' $file
 done
 
+# replace placeholders on nginx conf file
+sed -i 's/API_PROTOCOL/'${API_PROTOCOL}'/g' $NGINX_CONF_FILE
+sed -i 's/API_URL/'${API_URL}'/g' $NGINX_CONF_FILE
+
+# start nginx
 nginx -g 'daemon off;'
